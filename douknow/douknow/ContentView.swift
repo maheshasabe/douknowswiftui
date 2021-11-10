d44f62d362ed3411042fb39431ce0c9a79bfc19a
Mahesh Asabe <maheshasabe@gmail.com>
2021-11-10 12:36:11 +0530

    Initial xcode projected created

---
+++ douknow/douknow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  douknow
+//
+//  Created by Mahesh Asabe on 10/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ douknow/douknow/douknowApp.swift
@@ -0,0 +1,17 @@
+//
+//  douknowApp.swift
+//  douknow
+//
+//  Created by Mahesh Asabe on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct douknowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
